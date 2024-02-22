4df820464263cd6b4241bdd5f500229a5f668448
Syidik <syidik.priam@gmail.com>
2024-02-22 13:40:36 +0700

    add source code katalon aoutomation mobile

---
+++ Scripts/TC-SMS/Script1708578146806.groovy
@@ -0,0 +1,33 @@
+import static com.kms.katalon.core.checkpoint.CheckpointFactory.findCheckpoint
+import static com.kms.katalon.core.testcase.TestCaseFactory.findTestCase
+import static com.kms.katalon.core.testdata.TestDataFactory.findTestData
+import static com.kms.katalon.core.testobject.ObjectRepository.findTestObject
+import static com.kms.katalon.core.testobject.ObjectRepository.findWindowsObject
+import com.kms.katalon.core.checkpoint.Checkpoint as Checkpoint
+import com.kms.katalon.core.cucumber.keyword.CucumberBuiltinKeywords as CucumberKW
+import com.kms.katalon.core.mobile.keyword.MobileBuiltInKeywords as Mobile
+import com.kms.katalon.core.model.FailureHandling as FailureHandling
+import com.kms.katalon.core.testcase.TestCase as TestCase
+import com.kms.katalon.core.testdata.TestData as TestData
+import com.kms.katalon.core.testng.keyword.TestNGBuiltinKeywords as TestNGKW
+import com.kms.katalon.core.testobject.TestObject as TestObject
+import com.kms.katalon.core.webservice.keyword.WSBuiltInKeywords as WS
+import com.kms.katalon.core.webui.keyword.WebUiBuiltInKeywords as WebUI
+import com.kms.katalon.core.windows.keyword.WindowsBuiltinKeywords as Windows
+import internal.GlobalVariable as GlobalVariable
+import org.openqa.selenium.Keys as Keys
+
+Mobile.startApplication('E:\\APIDemos.apk', true)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.TextView - OS'), 0)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.TextView - SMS Messaging'), 0)
+
+Mobile.tap(findTestObject('Object Repository/android.widget.CheckBox - Enable SMS broadcast receiver'), 0)
+
+Mobile.setText(findTestObject('Object Repository/android.widget.EditText'), '081287165093', 0)
+
+Mobile.setText(findTestObject('Object Repository/android.widget.EditText (1)'), 'Syidik Priambodo', 0)
+
+Mobile.closeApplication()
+
